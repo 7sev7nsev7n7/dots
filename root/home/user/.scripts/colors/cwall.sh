bf7d8507ae230de566f9f71b6a682d4ab468b138
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-02-22 18:02:45 -0600

    swww will now fit images to screen instead of cropping them out, utilizing wal colors as a background color

--- root/home/user/.scripts/colors/cwall.sh
+++ root/home/user/.scripts/colors/cwall.sh
@@ -6,8 +6,8 @@ path=$(cd "$(dirname "$0")" && pwd) # script path
 file=$1
 
 # set wallpaper and color scheme
-$(swww img "$file" --transition-step 90 --transition-duration 1 --transition-type grow --transition-fps 144 &> /dev/null)
 wal -snti "$file"
+swww img "$file" --resize fit --fill-color $(sed '1!d' ~/.cache/wal/colors) --transition-step 90 --transition-duration 1 --transition-type grow --transition-fps 144 &> /dev/null
 
 # reload multiple items that require config updates
 $path/keyboard.sh 0
