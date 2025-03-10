df889e41daab8aeaa04ec26809ceecbe9ba04291
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-03-09 21:25:31 -0600

    Removed showing artist, since not every stream has one

--- root/home/user/.config/waybar/scripts/nowplaying.sh
+++ root/home/user/.config/waybar/scripts/nowplaying.sh
@@ -1,5 +1,5 @@
 #~/bin/bash
-out="$(playerctl metadata artist | sed -e 's/ - Topic//'): $(playerctl metadata title)"
+out="$(playerctl metadata title)"
 if [ "$out" == ": " ]; then
 	out="No media"
 fi
