2a45680f2a26da2a7dfdeed5113f43344e6d791b
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-03-27 20:02:44 -0600

    Changed way the turbocheck module works to utilize asusctl instead of directly accessing the profile file

--- root/home/user/.config/waybar/scripts/turbocheck.sh
+++ root/home/user/.config/waybar/scripts/turbocheck.sh
@@ -1,17 +1,9 @@
 #~/bin/bash
-turbo=$(cat /sys/devices/platform/asus-nb-wmi/throttle_thermal_policy)
+turbo=$(asusctl profile -p | tail -n 1 | sed -e "s/Active profile is //")
 curfile=$( cd $(dirname $0) && pwd)/current
 
-if [[ $turbo == 0 ]]; then
-	tt="Balanced"
-elif [[ $turbo == 1 ]]; then
-	tt="Turbo"
-elif [[ $turbo == 2 ]]; then
-	tt="Silent"
-fi
-
 if [[ "$turbo" != "$(cat $curfile)" ]]; then
-	dunstify -a "$HOSTNAME" "Changed performance mode to $tt"  &
+	dunstify -a "$HOSTNAME" "Changed governor mode to $turbo"  &
 fi
 echo $turbo > $curfile
-echo {\"alt\": \"$turbo\", \"tooltip\": \"$tt mode\",}
+echo {\"alt\": \"$turbo\", \"tooltip\": \"$turbo mode\",}
