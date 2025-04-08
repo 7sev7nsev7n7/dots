a39df51c1d65bf6f8be2d04d5c7c6fa6f4d32e52
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-04-08 16:02:32 -0600

    Updated color script to avoid reload waybar, as it already implements reloading colors on CSS style changes

--- root/home/user/.scripts/colors/cwall.sh
+++ root/home/user/.scripts/colors/cwall.sh
@@ -6,7 +6,7 @@ path=$(cd "$(dirname "$0")" && pwd) # script path
 file=$1
 
 # set wallpaper and color scheme
-wal -snti "$file" --cols16
+wal -nstei "$file" --cols16
 swww img "$file" --resize fit --fill-color $(sed '1!d' ~/.cache/wal/colors) --transition-step 90 --transition-duration 1 --transition-type grow --transition-fps 144 &> /dev/null
 
 # reload multiple items that require config updates
