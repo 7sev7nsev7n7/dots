ca6b721061a389d8b179047aa69a7d0ce8dcb0ec
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-06-23 23:39:33 -0600

    Updated stylings for Waybar, as well as changed the way some of the scripts for some modules worked

--- root/home/user/.config/waybar/scripts/nowplaying.sh
+++ root/home/user/.config/waybar/scripts/nowplaying.sh
@@ -1,6 +1,20 @@
 #~/bin/bash
-out="$(playerctl metadata artist) - $(playerctl metadata title)"
+
+# get values for artist and track title
+artist="$(playerctl metadata artist | sed -e 's/ - \(T\|t\)opic//')"
+title="$(playerctl metadata title)"
+
+# join artist and title
+out="$artist - $title"
+
+# check whether track is playing
 case $(playerctl status) in
-  Paused) out=" - $out";;
+  # show pause icon if track is paused
+  "Paused") out=" - $out"
+    ;;
+  # show nothing if track is not playing
+  "") out=""
+    ;;
 esac
+
 echo {\"text\": \"$out\"}
