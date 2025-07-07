07bcecf59befe0eca8d1dbdfcd71a9994611e36f
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-07-06 18:33:24 -0600

    Changed color used for setting keyboard backlight for keyboard.sh

--- root/home/user/.scripts/colors/keyboard.sh
+++ root/home/user/.scripts/colors/keyboard.sh
@@ -9,18 +9,17 @@ color=$3
 
 # variable checks
 if [[ -z "$@" ]]; then 
-	echo "no arguments passed"
-	exit
+  echo "no arguments passed"
+  exit
 fi
 
 # getting desired mode
 modes=('static' 'breathe' 'pulse' 'rainbow-cycle')
 current=$(cat $path/current)
 if [[ current+$1 -lt 0 || current+$1 -gt 3 ]] ; then # check if value of $1 is within accepted values
-	new=$((($current+$1)%4))
-	echo $new
+  new=$((($current+$1)%4))
 else
-	new=$(($current+$1))
+  new=$(($current+$1))
 fi
 echo $new > $path/current
 mode=${modes[$new]}
@@ -30,17 +29,19 @@ echo "changing mode to $mode"
 pow=$(cat /sys/devices/platform/asus-nb-wmi/leds/asus::kbd_backlight/brightness)
 pows=('off' 'low' 'med' 'high')
 case $mode in # if $mode is found in one of lists
-	'static'|'breathe'|'pulse')
-		col=4 # wal color to use for keyboard
-		if [[ -z $color ]]; then # if color argument is not found, use wallpaper color 
-			color=$(sed "$col q;d" ~/.cache/wal/colors | sed -e 's/#//')
-		fi
-		asusctl -k ${pows[$pow]} aura $mode -c $color
-		;;
-	'rainbow-cycle')
-		asusctl -k ${pows[$pow]} aura $mode
-		;;
-	*)
-		echo 'not found'
-		;;
+  'static'|'breathe'|'pulse')
+    if [[ -z $color ]]; then # if color argument is not found, use wallpaper color 
+      col=15 # wal color to use for keyboard
+      color=$(sed "$col q;d" ~/.cache/wal/colors | sed -e 's/#//')
+    fi
+    asusctl -k ${pows[$pow]} aura $mode -c $color
+    ;; 
+
+  'rainbow-cycle')
+    asusctl -k ${pows[$pow]} aura $mode
+    ;;
+
+  *)
+    echo 'not found'
+    ;;
 esac
