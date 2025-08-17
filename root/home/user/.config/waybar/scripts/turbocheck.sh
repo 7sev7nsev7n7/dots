a1d2bc9e170e98da8edfa1813514d39a5400bd22
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-08-16 19:38:31 -0600

    Moved custom/turbo to main waybar modules, and disabled dunst notifications upon changing modes

--- root/home/user/.config/waybar/scripts/turbocheck.sh
+++ root/home/user/.config/waybar/scripts/turbocheck.sh
@@ -2,8 +2,5 @@
 turbo=$(asusctl profile -p | tail -n 1 | sed -e "s/Active profile is //")
 curfile=$( cd $(dirname $0) && pwd)/current
 
-if [[ "$turbo" != "$(cat $curfile)" ]]; then
-	dunstify -a "$HOSTNAME" "Changed governor mode to $turbo"  &
-fi
 echo $turbo > $curfile
 echo {\"alt\": \"$turbo\", \"tooltip\": \"$turbo mode\",}
