11a6cae676f99ac1f424100c48fe1c0a5c5e7af7
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-11-20 19:45:07 -0600

    Changed windowshot script to include window borders (sizes will need to be adjustable in the future)

--- root/home/user/.scripts/hypr/windowshot.sh
+++ root/home/user/.scripts/hypr/windowshot.sh
@@ -1,12 +1,12 @@
-# get window dimensions (start/end coords for flameshot)
+# get window position and dimensions
 raw="$(hyprctl activewindow)"
 pos=(
-  "$(sed -e '1,3d;5,$d;s/\s*at: //;s/,.*//' <<<$raw )"
-  "$(sed -e '1,3d;5,$d;s/.*,//' <<<$raw )"
+  $((-4+"$(sed -e '1,3d;5,$d;s/\s*at: //;s/,.*//' <<<$raw )"))
+  $((-4+"$(sed -e '1,3d;5,$d;s/.*,//' <<<$raw )"))
 )
 size=(
-  "$(sed -e '1,4d;6,$d;s/\s*size: //;s/,.*//' <<<$raw )"
-  "$(sed -e '1,4d;6,$d;s/.*,//' <<<$raw )"
+  $((8+"$(sed -e '1,4d;6,$d;s/\s*size: //;s/,.*//' <<<$raw )"))
+  $((8+"$(sed -e '1,4d;6,$d;s/.*,//' <<<$raw )"))
 )
 region="${size[0]}x${size[1]}+${pos[0]}+${pos[1]}"
 
