91117e7379e636161eb67049170ad30c8ccd643c
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-12-31 07:40:29 -0600

    Increased contrast for colors used by wal, improving readability and appearance

--- root/home/user/.scripts/colors/cwall.sh
+++ root/home/user/.scripts/colors/cwall.sh
@@ -5,12 +5,12 @@ path=$(cd "$(dirname "$0")" && pwd) # script path
 file=$1
 
 # set wallpaper and color scheme
-wal -nstei "$file" --cols16 darken --contrast 2
-swww img "$file" --transition-type none # --fill-color $(sed '1!d' ~/.cache/wal/colors)
+wal -nstei "$file" --cols16 darken --contrast 5
+swww img "$file" --transition-type none # --fill-color $(sed '1!d' ~/.cache/wal/colors) &
 
 # reload multiple items that require config updates
 $path/keyboard.sh 0
-pkill -SIGUSR1 kitty
+pkill -SIGUSR1 kitty &
 pywal-discord -t universal &
 pywalfox update &
 gsettings set org.gnome.desktop.interface gtk-theme Adawaita && gsettings set org.gnome.desktop.interface gtk-theme Universal &
