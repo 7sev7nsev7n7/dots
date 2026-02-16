3d35324e41291173aa5cf150371109ab82b85b26
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2026-02-16 17:07:23 -0600

    Reintroduced tiled window gaps, rounding and transparency for most graphical interfaces (yay aero resurface)

--- root/home/user/.scripts/colors/cwall.sh
+++ root/home/user/.scripts/colors/cwall.sh
@@ -5,7 +5,7 @@ path=$(cd "$(dirname "$0")" && pwd) # script path
 file=$1
 
 # set wallpaper and color scheme
-wal -nstei "$file" --cols16 lighten --contrast 2 --backend colorthief
+wal -nstei "$file" --cols16 lighten --backend colorthief 
 swww img "$file" --transition-type none
 
 # reload multiple items that require config updates
@@ -14,6 +14,8 @@ pkill -SIGUSR1 kitty
 pywal-discord -t universal
 pywalfox update
 gsettings set org.gnome.desktop.interface gtk-theme Adawaita && gsettings set org.gnome.desktop.interface gtk-theme Universal
+pkill dunst
+$(xrdb -load  ~/.Xresources)
 
 # check if files are softlinked, create softlinks otherwise
 sources=(~/.cache/wal/colors-swaylock ~/.cache/wal/dunstrc ~/.cache/wal/Xresources) # source configuration file
@@ -29,6 +31,3 @@ for i in $(seq 0 $((${#sources[@]}-1))); do
 	fi
 	ln -s ${sources[$i]} ${destinations[$i]}; echo "created softlink from ${sources[$i]} at ${destinations[$i]}"
 done
-
-pkill dunst
-$(xrdb -load  ~/.Xresources)&
