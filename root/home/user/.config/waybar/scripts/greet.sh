a5d1cac361db7291097dfcf72b7c31c61b9684ae
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2026-03-05 23:34:54 -0600

    Changed location of MOTD list used by Waybar, as this list is planned to be used for hyprlock as well, so having it in a more commonly accessible location makes more sense now

--- root/home/user/.config/waybar/scripts/greet.sh
+++ root/home/user/.config/waybar/scripts/greet.sh
@@ -1,4 +1,4 @@
 #~/bin/bash
-out="$HOSTNAME: $(shuf -n 1 ~/.config/waybar/scripts/dependencies/motd)"
+out="$HOSTNAME: $(shuf -n 1 ~/.scripts/hypr/motd)"
 tt=$(shuf -n 1 ~/.config/waybar/scripts/dependencies/tooltip)
 echo {\"text\": \"$out\", \"tooltip\": \"$tt\"}
